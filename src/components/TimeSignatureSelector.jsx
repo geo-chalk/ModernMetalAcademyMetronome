e2cfa4671d541d6b9afced42ca0bf9c04b61b4f0
georgios.chalkiopoul <geo.chalkiopoulos@gmail.com>
2026-04-02 12:27:28 +0300

    add more time signatures

--- src/components/TimeSignatureSelector.jsx
+++ src/components/TimeSignatureSelector.jsx
@@ -17,7 +17,7 @@ const TimeSignatureSelector = ({top, bottom, setTop, setBottom, isActive}) => {
                         disabled={isActive}
                         className="bg-transparent text-white text-3xl font-black focus:outline-none appearance-none cursor-pointer text-center z-10 pr-4 leading-none"
                     >
-                        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 12].map(num => (
+                        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(num => (
                             <option key={num} value={num} className="bg-[#1E1E1E]">{num}</option>
                         ))}
                     </select>
